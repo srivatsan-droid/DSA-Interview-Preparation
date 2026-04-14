fdc9a2bf75de65e44f64f52a5ba9fd387d43a85f
Srivatsan <sriramsrivatsan2001@gmail.com>
2026-04-14 12:30:12 +0530

    Two Pointers | Valid Palindrome - Day 3

---
+++ Blind_75/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,22 @@
+package Blind_75.TwoPointers;
+
+public class ValidPalindrome {
+    public static boolean isValid(String s) {
+        String cleanedString = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int left = 0, right = cleanedString.length() - 1;
+        while (left <= right) {
+            if (cleanedString.charAt(left) != cleanedString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "race a car";
+        boolean ans = isValid(s);
+        System.out.println(ans);
+    }
+}
